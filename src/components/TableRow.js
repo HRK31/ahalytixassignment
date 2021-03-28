3213265ad15029410fe1d97174052e0be09ab90c
Hritik Kumar <hrk10321@gmail.com>
2021-03-28 15:05:39 +0530

    Added change of one state to another state on click on Show Detail to show details of Crypto Currencies and hide it on another click again

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,14 +1,33 @@
 import { Component } from "react";
+import RowDetail from "./RowDetail";
 
 class TableRow extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            displayComponent: false,
+        }
+        this.execute = this.execute.bind(this);
+    }
+
+    execute() {
+        this.setState({
+            displayComponent: !this.state.displayComponent,
+        })
+    }
+
     render() {
         return (
 
             <tr>
-                <td>{this.props.item.id}</td>
+                <td >{this.props.sno}</td>
                 <td>{this.props.item.name}</td>
                 <td>{this.props.item.symbol}</td>
+                <td><a href="#" onClick={this.execute}>Show Details</a>
+                    {this.state.displayComponent ? <RowDetail id={this.props.item.id} /> : null}
+                </td>
             </tr>
+
 
         );
     }
